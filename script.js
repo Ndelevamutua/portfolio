ca8cd19a92a5aee9ba15e1f09de9f17a1856afe0
Ndelevamutua <146925708+Ndelevamutua@users.noreply.github.com>
2024-03-18 16:32:32 +0300

    script.js

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-const sidebar = document.querySelector(".sidebar");
-const sidebarClose = document.querySelector("#sidebar-close");
-const menu = document.querySelector(".menu-content");
-const menuItems = document.querySelectorAll(".submenu-item");
-const subMenuTitles = document.querySelectorAll(".submenu .menu-title");
 
-sidebarClose.addEventListener("click", () => sidebar.classList.toggle("close"));
+  document.addEventListener("DOMContentLoaded", function() {
+    const sidebar = document.querySelector(".sidebar");
+    const sidebarToggle = document.querySelector("#sidebar-close");
 
-menuItems.forEach((item, index) => {
-  item.addEventListener("click", () => {
-    menu.classList.add("submenu-active");
-    item.classList.add("show-submenu");
-    menuItems.forEach((item2, index2) => {
-      if (index !== index2) {
-        item2.classList.remove("show-submenu");
-      }
+    // Close the sidebar by default
+    sidebar.classList.add("close");
+
+    sidebarToggle.addEventListener("click", () => {
+      sidebar.classList.toggle("close");
+    });
+
+    const menuItems = document.querySelectorAll(".submenu-item");
+
+    menuItems.forEach((menuItem) => {
+      menuItem.addEventListener("click", () => {
+        sidebar.classList.add("close");
+      });
     });
   });
-});
-
-subMenuTitles.forEach((title) => {
-  title.addEventListener("click", () => {
-    menu.classList.remove("submenu-active");
-  });
-});
-
-console.log(menuItems, subMenuTitles);
